ad7d99e163468fb85e6bf740198a3f48460e3d45
B0redProgrammer <incandelha@gmail.com>
2022-08-30 23:10:51 +0200

    Managed to init the playing field xD

--- tictactoe.cs
+++ tictactoe.cs
@@ -2,26 +2,27 @@ using System;
 
 namespace Project_1 {
     class MainClass {
-        int [] playingfield = new int[3][3];
+        static int[,] playingfield = new int[3, 3];
 
-        public void initField() {
-          foreach(int i in playingfield) {
-            foreach(int j in playingfield[i]) {
-              this.playingfield[i][j] = 0;
+        public static void initField() {
+          for(int i = 0; i<playingfield.GetLength(0); i++) {
+            for(int j = 0; j<playingfield.GetLength(1); j++) {
+              playingfield[i, j] = 0;
             }
           }
         }
 
         public static void Main (string[] args) {
             initField();
-            Console.ReadKey ();
+            Console.ReadKey();
         }
 
-        public bool CheckWinnings(int [] board) {
+        public static bool CheckWinnings(int [] board) {
 
         }
 
-        public void minimax(int [] board, int depth, bool MaxinPlayer) {
+
+        public static void minimax(int [] board, int depth, bool MaxinPlayer) {
 
         }
     }
